6f0b3062fe4cac1abaf8ad56d3844f7c61be1026
Peng Xiao <pengxiao@outlook.com>
2022-03-29 11:19:49 +0800

    auto commit

--- logseq/custom.js
+++ logseq/custom.js
@@ -8,21 +8,45 @@ function isEditing() {
   return logseq.api.check_editing();
 }
 
-function getSelectedTextarea() {
+function getActiveTextarea() {
   if (isEditing()) {
-    const selection = window.getSelection();
-    if (selection.toString()) {
-      const textarea = document.activeElement;
-      if (textarea.nodeName === 'TEXTAREA' && selection.anchorNode.contains(textarea)) {
-        return textarea;
-      }
+    const textarea = document.activeElement;
+    return textarea.nodeName === "TEXTAREA" ? textarea : undefined;
+  }
+}
+
+function quoteTextInTextarea(textarea, quote) {
+  if (isEditing()) {
+    const { selectionStart, selectionEnd } = textarea;
+    const selectedText = textarea.value.substring(selectionStart, selectionEnd);
+    if (selectedText) {
+      textarea.value =
+        textarea.value.substring(0, selectionStart) +
+        quote +
+        selectedText +
+        quote +
+        textarea.value.substring(selectionEnd);
+      textarea.selectionStart = selectionStart;
+      textarea.selectionEnd = selectionEnd + 2;
+      return true;
     }
   }
 }
 
-window.addEventListener("keydown", (e) => {
-  if (isActive()) {
-    const textarea = getSelectedTextarea();
-    console.log(textarea);
-  }
-});
+window.addEventListener(
+  "keydown",
+  (e) => {
+    if (isActive() && e.key === '"') {
+      const textarea = getActiveTextarea();
+      if (textarea) {
+        const quoted = quoteTextInTextarea(textarea, '"');
+        console.log(quoted);
+        if (quoted) {
+          e.stopPropagation();
+          e.preventDefault();
+        }
+      }
+    }
+  },
+  true
+);
